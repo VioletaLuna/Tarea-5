dddca027a08bceb6fef3b25aa5b8808414a651e6
Violeta Rodríguez <violetar48@gmail.com>
2017-12-03 07:25:53 -0500

    Con leyend

--- Plots.py
+++ Plots.py
@@ -7,6 +7,9 @@ r = data[:,0]
 velExp = data[:,1]
 velModel =data[:,2]
 
-plt.scatter(r,velExp)
-plt.plot(r,velModel)
+plt.scatter(r,velExp, label = "Datos de velocidades")
+plt.plot(r,velModel, label = "Velocidades del modelo", c = "r")
+plt.xlabel("Radio (kpc)")
+plt.ylabel("Velocidad (km/s)")
+plt.legend(loc="upper left")
 plt.savefig("grafica.png")
